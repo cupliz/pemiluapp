253767b7daf94ff50223c3db82d0dfeefde93738
resaUndefined <resamuhammad96@gmail.com>
2018-12-14 10:38:06 +0700

    starter front end react next js

---
+++ pages/admin/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+
+class Index extends Component{
+	render(){
+		return(
+			<div>
+				Halaman Admin
+			</div>
+			)
+	}
+}
+export default Index
---
+++ pages/vote/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+
+class Index extends Component{
+	render(){
+		return(
+			<div>
+				Page Pilihan
+			</div>
+			)
+	}
+}
+export default Index
